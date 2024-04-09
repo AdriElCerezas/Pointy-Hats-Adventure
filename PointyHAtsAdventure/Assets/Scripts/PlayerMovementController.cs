61d9f2c73d5291886b39e4f1279c3ad24044561d
AdriElCerezas <adelaiglesiaf@edu.tecnocampus.cat>
2024-04-09 14:29:17 +0200

    0.1

---
+++ PointyHAtsAdventure/Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,164 @@
+/*using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    Vector2 moveInput;
+    Vector2 movement;
+    public Stats playerStats;
+    float moveSpeed;
+    Rigidbody2D rb;
+
+    // Dash:
+    public float dashSpeed = 20f;
+    public bool dashAvilable = true;
+    public float dashDuration = 0.1f; // Duración del dash reducida
+    float dashTimer;
+
+    void Start()
+    {
+        moveSpeed = playerStats.speed;
+        rb = GetComponent<Rigidbody2D>();
+        dashTimer = dashDuration;
+    }
+    void Update()
+    {
+        if (!dashAvilable)
+        {
+            dashTimer -= Time.deltaTime;
+            if (dashTimer <= 0)
+            {
+                dashAvilable = true;
+            }
+        }
+    }
+    public void Move(InputAction.CallbackContext ctx)
+    {
+            moveInput = ctx.ReadValue<Vector2>();
+            movement = new Vector2(moveInput.x, moveInput.y).normalized;
+
+            if (movement != Vector2.zero)
+            {
+                rb.velocity = movement * moveSpeed;
+            }
+            else
+            {
+                rb.velocity = Vector2.zero;
+            }
+    }
+    public void Dash(InputAction.CallbackContext ctx)
+    {
+        if (dashAvilable)
+        {
+            dashAvilable = false;
+
+            // Calcula la dirección del dash
+            Vector2 dashDirection = movement.normalized;
+
+            // Aplica una fuerza al Rigidbody2D en la dirección del dash
+            rb.AddForce(dashDirection * dashSpeed, ForceMode2D.Impulse);
+            rb.velocity = movement.normalized * dashSpeed;
+        }
+    }
+}
+*/
+using UnityEngine.InputSystem;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    //Movimiento
+    Vector2 moveInput;
+    Vector2 movement;
+    public Stats playerStats;
+    float moveSpeed;
+    public Rigidbody2D rb;
+
+    //Dash
+    public float baseDashSpeed = 20f;
+    float dashSpeed;
+    public bool isDashing = false;
+    Vector2 dashDirection;
+    public float dashDuration = 0.1f;
+    float dashTimer;
+
+    public bool dashAvilable = true;
+    public float cooldownDuration = 0.3f;
+    float cooldownTimer;
+
+    void Start()
+    {
+        //Añadir a camara
+
+        //Movimiento
+        moveSpeed = playerStats.baseSpeed;
+        dashSpeed = baseDashSpeed;
+        dashTimer = 0f;
+        cooldownTimer = cooldownDuration;
+    }
+    void Update()
+    {
+        if(isDashing)
+        {
+            dashTimer -= Time.deltaTime;
+            if(dashTimer <= 0 )
+            {
+                rb.AddForce(-dashDirection * dashSpeed, ForceMode2D.Impulse);
+                isDashing = false;
+            }
+        }
+        if(!dashAvilable)
+        {
+            cooldownTimer -= Time.deltaTime;
+            if (cooldownTimer <= 0)
+            {
+                dashAvilable = true;
+            }
+        }
+    }
+    public void Move(InputAction.CallbackContext ctx)
+    {
+        if (!isDashing)
+        {
+            moveInput = ctx.ReadValue<Vector2>();
+            movement = new Vector2(moveInput.x, moveInput.y).normalized;
+
+            // Si el jugador no es invulnerable, moverse con normalidad
+            if (movement != Vector2.zero)
+            {
+                rb.velocity = movement * moveSpeed;
+            }
+            else
+            {
+                rb.velocity = Vector2.zero;
+            }
+        }
+    }
+    public void Dash(InputAction.CallbackContext ctx)
+    {
+        if(ctx.started && !isDashing && dashAvilable)
+        {
+            dashDirection = moveInput.normalized;
+            isDashing = true;
+            dashAvilable = false;
+            rb.AddForce(dashDirection * dashSpeed, ForceMode2D.Impulse);
+            dashTimer = dashDuration;
+            cooldownTimer = cooldownDuration;
+        }
+    }
+    public void Freeze(InputAction.CallbackContext ctx)
+    {
+        if (playerStats.isFreezed)
+        {
+            playerStats.isFreezed = false;
+            moveSpeed = playerStats.baseSpeed;
+            dashSpeed = baseDashSpeed;
+        } else
+        {
+            moveSpeed = playerStats.baseSpeed / 2;
+            dashSpeed = baseDashSpeed / 2;
+            playerStats.isFreezed = true;
+        }
+    }
+}
---
+++ PointyHAtsAdventure/Assets/Scripts/Stats.cs
@@ -0,0 +1,131 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Game/Stats")]
+public class Stats : ScriptableObject
+{
+    public int life;
+    public int baseSpeed;
+    public int speed;
+    public int acuracy;
+    public int amo;
+    public int maxAmo;
+    public int charger;
+    public float rangeRadius;
+    public float fireRate;
+    public bool isFreezed;
+    public bool isBurning;
+    public bool isPoisoned;
+    public bool facingRight;
+    bool test;
+
+    public void Update()
+    {
+        if (isFreezed)
+        {
+            speed = baseSpeed / 2;
+        } else
+        {
+            speed = baseSpeed;
+        }
+    }
+}
+
+    /*public int GetLife()
+    {
+        return life;
+    }
+    public int GetSpeed()
+    {
+        return speed;
+    }
+    public int GetAccuracy()
+    {
+        return acuracy;
+    }
+    public int GetAmo()
+    {
+        return amo;
+    }
+    public int GetMaxAmo()
+    {
+        return maxAmo;
+    }
+    public int GetCharger()
+    {
+        return charger;
+    }
+    public float GetRangeRadius()
+    {
+        return rangeRadius;
+    }
+    public float GetFireRate()
+    {
+        return fireRate;
+    }
+    public bool IsFreezed()
+    {
+        return isFreezed;
+    }
+    public bool IsBurning()
+    {
+        return isBurning;
+    }
+    public bool IsPoisoned()
+    {
+        return isPoisoned;
+    }
+    public bool IsFacingRight()
+    {
+        return facingRight;
+    }
+    public void SetLife(int value)
+    {
+        life = value;
+    }
+    public void SetSpeed(int value)
+    {
+        speed = value;
+    }
+    public void SetAccuracy(int value)
+    {
+        acuracy = value;
+    }
+    public void SetAmo(int value)
+    {
+        amo = value;
+    }
+    public void SetMaxAmo(int value)
+    {
+        maxAmo = value;
+    }
+    public void SetCharger(int value)
+    {
+        charger = value;
+    }
+    public void SetRangeRadius(float value)
+    {
+        rangeRadius = value;
+    }
+    public void SetFireRate(float value)
+    {
+        fireRate = value;
+    }
+    public void SetFreezed(bool value)
+    {
+        isFreezed = value;
+    }
+    public void SetBurning(bool value)
+    {
+        isBurning = value;
+    }
+    public void SetPoisoned(bool value)
+    {
+        isPoisoned = value;
+    }
+    public void SetFacingRight(bool value)
+    {
+        facingRight = value;
+    }
+
+}
+*/
